1cb0d4c9391608349ddde0d70fd146bd823e2ed1
rabah1749 <77684146+rabah1749@users.noreply.github.com>
2021-05-31 20:55:53 +0530

    Add files via upload

--- C81main.js
+++ C81main.js
@@ -1,33 +1,37 @@
 canvas=document.getElementById("myCanvas");
 ctx=canvas.getContext("2d");
 
+color="yellow";
+ctx.beginPath();
+ctx.strokeStyle = color;
+ctx.lineWidth = 2;
+ctx.arc(300, 250, 40 ,0 , 2*Math.PI);
+ctx.stroke();
+
+color="blue";
+ctx.beginPath();
+ctx.strokeStyle = color;
+ctx.lineWidth = 2;
+ctx.arc(250,210, 40 ,0 , 2*Math.PI);
+ctx.stroke();
+
+color="black";
+ctx.beginPath();
+ctx.strokeStyle = color;
+ctx.lineWidth = 2;
+ctx.arc(350, 210, 40 ,0 , 2*Math.PI);
+ctx.stroke();
+
 color="red";
 ctx.beginPath();
 ctx.strokeStyle = color;
 ctx.lineWidth = 2;
-ctx.arc(200, 200, 40 ,0 , 2*Math.PI);
+ctx.arc(450,210, 40 ,0 , 2*Math.PI);
 ctx.stroke();
 
+color="green";
 ctx.beginPath();
 ctx.strokeStyle = color;
 ctx.lineWidth = 2;
-ctx.arc(400, 400, 40 ,0 , 2*Math.PI);
-ctx.stroke();
-
-ctx.beginPath();
-ctx.strokeStyle = color;
-ctx.lineWidth = 2;
-ctx.arc(145, 145, 40 ,0 , 2*Math.PI);
-ctx.stroke();
-
-ctx.beginPath();
-ctx.strokeStyle = color;
-ctx.lineWidth = 2;
-ctx.arc(350,350, 40 ,0 , 2*Math.PI);
-ctx.stroke();
-
-ctx.beginPath();
-ctx.strokeStyle = color;
-ctx.lineWidth = 2;
-ctx.arc(50,50, 40 ,0 , 2*Math.PI);
+ctx.arc(400, 250, 40 ,0 , 2*Math.PI);
 ctx.stroke();
